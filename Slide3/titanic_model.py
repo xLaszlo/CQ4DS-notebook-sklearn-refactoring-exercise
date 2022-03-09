57bf5641edd20c1118840b5ae3308dbba739bdbd
Laszlo Sragner <sragner@gmail.com>
2022-03-09 12:24:57 +0000

    prep project

---
+++ Slide3/titanic_model.py
@@ -0,0 +1,16 @@
+import typer
+
+class TitanicModelCreator:
+
+  def __init__(self):
+    pass
+
+  def run(self):
+    print('Hello World!')
+
+def main(param: str='pass'):
+  titanicModelCreator = TitanicModelCreator()
+  titanicModelCreator.run()
+
+if __name__ == "__main__":
+  typer.run(main)
