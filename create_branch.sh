98d29110ce80e033b8cc4572e25a76530b80b635
Laszlo Sragner <sragner@gmail.com>
2022-03-09 12:47:56 +0000

    t1

--- create_branch.sh
+++ create_branch.sh
@@ -4,10 +4,10 @@ switch=$1
 if [ -z "$switch" ]; then
     echo "use ./create_branch -do"
 else
-    cp -r Slide2 Slide1
-    cp -r Slide3 Slide2
-    cp -r Slide4 Slide3
-    cp -r Slide5 Slide4
+    cp Slide2/* Slide1
+    cp Slide3/* Slide2
+    cp Slide4/* Slide3
+    cp Slide5/* Slide4
 fi
 
 
