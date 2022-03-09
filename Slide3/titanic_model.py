defaa671cda77cac80c688f7afc9d3d26404c222
Laszlo Sragner <sragner@gmail.com>
2022-03-09 12:32:23 +0000

    notebook in script

--- Slide3/titanic_model.py
+++ Slide3/titanic_model.py
@@ -2,15 +2,15 @@ import typer
 
 class TitanicModelCreator:
 
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def run(self):
-    print('Hello World!')
+    def run(self):
+        print('Hello World!')
 
 def main(param: str='pass'):
-  titanicModelCreator = TitanicModelCreator()
-  titanicModelCreator.run()
+    titanicModelCreator = TitanicModelCreator()
+    titanicModelCreator.run()
 
 if __name__ == "__main__":
-  typer.run(main)
+    typer.run(main)
