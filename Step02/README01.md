ff121044ca091e3884a05b5e908771f5b27f7052
Laszlo Sragner <sragner@gmail.com>
2022-03-10 12:02:15 +0000

    readmes

---
+++ Step02/README01.md
@@ -0,0 +1,4 @@
+## Step 01: Project setup
+
+- Write script to create virtual environment
+- Write the first `requirements.txt`
--- create_branch.sh
+++ create_branch.sh
@@ -4,25 +4,25 @@ switch=$1
 if [ -z "$switch" ]; then
     echo "use ./create_branch -do"
 else
-    cp Slide02/* Slide01
-    cp Slide03/* Slide02
-    cp Slide04/* Slide03
-    cp Slide05/* Slide04
-    cp Slide06/* Slide05
-    cp Slide07/* Slide06
-    cp Slide08/* Slide07
-    cp Slide09/* Slide08
-    cp Slide10/* Slide09
-    cp Slide11/* Slide10
-    cp Slide12/* Slide11
-    cp Slide13/* Slide12
-    cp Slide14/* Slide13
-    cp Slide15/* Slide14
-    cp Slide16/* Slide15
-    cp Slide17/* Slide16
-    cp Slide18/* Slide17
-    cp Slide19/* Slide18
-    cp Slide20/* Slide19
-    cp Slide21/* Slide20
-    cp Slide22/* Slide21
+    cp Step02/* Step01
+    cp Step03/* Step02
+    cp Step04/* Step03
+    cp Step05/* Step04
+    cp Step06/* Step05
+    cp Step07/* Step06
+    cp Step08/* Step07
+    cp Step09/* Step08
+    cp Step10/* Step09
+    cp Step11/* Step10
+    cp Step12/* Step11
+    cp Step13/* Step12
+    cp Step14/* Step13
+    cp Step15/* Step14
+    cp Step16/* Step15
+    cp Step17/* Step16
+    cp Step18/* Step17
+    cp Step19/* Step18
+    cp Step20/* Step19
+    cp Step21/* Step20
+    cp Step22/* Step21
 fi
